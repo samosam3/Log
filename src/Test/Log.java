5937320ad4740dd84943a348ed5a13fb1a0ef85c
samosam3 <58535630+samosam3@users.noreply.github.com>
2020-02-29 14:58:24 -0500

    Update Log.java

--- src/Test/Log.java
+++ src/Test/Log.java
@@ -3,6 +3,10 @@ package Test;
 public class Log {
     public static void main(String[] args) {
         System.out.println("Message");
+        
+        // lets update ........
+        
+        
     }
 }
 
